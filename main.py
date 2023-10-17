588c83853b039bb7bd6278967cc4432f096347b9
kody.low <kodylow7@gmail.com>
2023-10-16 19:30:32 -0700

    update to be a flask app

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 from github import Github, Auth
-import argparse
+from git import Repo
+from flask import Flask, request, render_template, send_file
 from github_client import fetch_issues_and_prs
 from utils import save_to_markdown
-
-# Add argument parsing
-parser = argparse.ArgumentParser(description="Fetch issues and PRs from a GitHub repo.")
-parser.add_argument(
-    "-d", "--days", type=int, default=7, help="Number of days to filter by."
-)
-args = parser.parse_args()
-days = args.days
+import tempfile
+import time
 
 load_dotenv()  # take environment variables from .env.
+
+# Get environment variables
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -23,25 +21,64 @@ logger = logging.getLogger(__name__)
 # Get today's date
 today = datetime.now().strftime("%B %d, %Y")
 
-# Create a directory with today's date
-os.makedirs(today, exist_ok=True)
+app = Flask(__name__)
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-auth = Auth.Token(GITHUB_TOKEN)
-OWNER = "fedimint"
-REPO_NAME = "fedimint"
 
-logger.info("Initializing Github client...")
-g = Github(auth=auth)
-repo = g.get_repo(f"{OWNER}/{REPO_NAME}")
-logger.info("Github client initialized successfully.")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        repo_link = request.form.get("repo_link")
+        days = int(request.form.get("days"))
+        logger.info("Starting script...")
+
+        # Parse owner and repo name from the repo_link
+        OWNER, REPO_NAME = repo_link.split("/")[-2:]
+
+        # Initialize Github client and get the repo
+        start_time = time.time()
+        auth = Auth.Token(GITHUB_TOKEN)
+        g = Github(auth=auth)
+        repo = g.get_repo(f"{OWNER}/{REPO_NAME}")
+        end_time = time.time()
+        print(
+            f"Time taken for Github client and repo initialization: {end_time - start_time} seconds"
+        )
+
+        # Create a temporary directory
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # Perform your operations on the repo
+            start_time = time.time()
+            issues, prs = fetch_issues_and_prs(logger, today, days, repo)
+            end_time = time.time()
+            print(
+                f"Time taken for fetch_issues_and_prs: {end_time - start_time} seconds"
+            )
+
+            issues = sorted(issues, key=lambda i: i.updated_at, reverse=True)
+            prs = sorted(prs, key=lambda p: p.updated_at, reverse=True)
+
+            # Save to markdown
+            start_time = time.time()
+            markdown_content = save_to_markdown(
+                logger,
+                today,
+                issues,
+                prs,
+                graph_filename="graphs.png",
+                directory=tmpdirname,
+            )
+            end_time = time.time()
+            print(f"Time taken for save_to_markdown: {end_time - start_time} seconds")
+
+            # Return the markdown content as a response
+            return markdown_content
+
+        # The temporary directory and its contents are automatically deleted here
+
+        return response
+
+    return render_template("index.html")  # render a form for input
 
 
 if __name__ == "__main__":
-    logger.info("Starting script...")
-    issues, prs = fetch_issues_and_prs(logger, today, days, repo)
-    issues = sorted(issues, key=lambda i: i.updated_at, reverse=True)
-    prs = sorted(prs, key=lambda p: p.updated_at, reverse=True)
-    save_to_markdown(logger, today, issues, prs, graph_filename="graphs.png")
-    logger.info("Data dumped to issues_and_prs.md successfully!")
-    logger.info("Script finished.")
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,70 +1,77 @@
 from data_processing import generate_graphs_and_statistics
 
 
-def write_to_file(f, items, state, item_type):
-    f.write(f"\n\n# {state} {item_type}\n\n")
-    for item in items:
-        f.write(
-            f"- {item.number}: [{item.title}]({item.html_url}) - ({item.updated_at})\n"
+# def write_to_file(f, items, state, item_type):
+#     f.write(f"\n\n# {state} {item_type}\n\n")
+#     for item in items:
+#         f.write(
+#             f"- {item.number}: [{item.title}]({item.html_url}) - ({item.updated_at})\n"
+#         )
+
+# labels = ", ".join([label.name for label in item.labels])
+# f.write(f"<details>\n")
+# f.write(f"<summary>{item.number}: [{item.title}]({item.html_url})</summary>\n")
+# f.write(f"<ul>\n")
+# f.write(f"  <li>User: {item.user.login}</li>\n")
+# f.write(f"  <li>State: {item.state}</li>\n")
+# f.write(f"  <li>Created At: {item.created_at}</li>\n")
+# f.write(f"  <li>Updated At: {item.updated_at}</li>\n")
+# f.write(f"  <li>Comments: {item.comments}</li>\n")
+# f.write(f"  <li>Labels: {labels}</li>\n")
+# f.write(f"</ul>\n")
+# f.write(f"</details>\n\n")
+
+# f.write(f"\n\n# {state} {item_type}\n\n")
+# f.write(
+#     "| Number | Title | User | State | Created At | Updated At | Comments | Labels |\n"
+# )
+# f.write(
+#     "|--------|-------|------|-------|------------|------------|----------|--------|\n"
+# )
+# for item in items:
+#     labels = ", ".join([label.name for label in item.labels])
+#     f.write(
+#         f"| #{item.number} | [{item.title}]({item.html_url}) | {item.user.login} | {item.state} | {item.created_at} | {item.updated_at} | {item.comments} | {labels} |\n"
+#     )
+
+# Add more useful information to the markdown
+# f.write(f"\n\n## Statistics for {state} {item_type}\n\n")
+# f.write(f"Total number: {len(items)}\n\n")
+
+# users = [item.user.login for item in items]
+# user_counts = {user: users.count(user) for user in set(users)}
+# f.write("Number of items per user:\n\n")
+# for user, count in user_counts.items():
+#     f.write(f"- {user}: {count}\n")
+
+
+def save_to_markdown(logger, today, issues, prs, graph_filename, directory):
+    logger.info("Generating markdown for issues and pull requests...")
+
+    markdown_content = ""
+
+    # Uncomment the following lines if you want to include graphs and statistics in your markdown
+    # markdown_content += "\n\n## Graphs\n\n"
+    # markdown_content += f"![Issues and PRs by User](./{graph_filename})\n\n"
+
+    for state in ["Open", "Closed"]:
+        markdown_content += write_to_file(
+            [i for i in issues if i.state.lower() == state.lower()],
+            state,
+            "Issues",
+        )
+        markdown_content += write_to_file(
+            [p for p in prs if p.state.lower() == state.lower()],
+            state,
+            "Pull Requests",
         )
 
-        # labels = ", ".join([label.name for label in item.labels])
-        # f.write(f"<details>\n")
-        # f.write(f"<summary>{item.number}: [{item.title}]({item.html_url})</summary>\n")
-        # f.write(f"<ul>\n")
-        # f.write(f"  <li>User: {item.user.login}</li>\n")
-        # f.write(f"  <li>State: {item.state}</li>\n")
-        # f.write(f"  <li>Created At: {item.created_at}</li>\n")
-        # f.write(f"  <li>Updated At: {item.updated_at}</li>\n")
-        # f.write(f"  <li>Comments: {item.comments}</li>\n")
-        # f.write(f"  <li>Labels: {labels}</li>\n")
-        # f.write(f"</ul>\n")
-        # f.write(f"</details>\n\n")
-
-    # f.write(f"\n\n# {state} {item_type}\n\n")
-    # f.write(
-    #     "| Number | Title | User | State | Created At | Updated At | Comments | Labels |\n"
-    # )
-    # f.write(
-    #     "|--------|-------|------|-------|------------|------------|----------|--------|\n"
-    # )
-    # for item in items:
-    #     labels = ", ".join([label.name for label in item.labels])
-    #     f.write(
-    #         f"| #{item.number} | [{item.title}]({item.html_url}) | {item.user.login} | {item.state} | {item.created_at} | {item.updated_at} | {item.comments} | {labels} |\n"
-    #     )
-
-    # Add more useful information to the markdown
-    # f.write(f"\n\n## Statistics for {state} {item_type}\n\n")
-    # f.write(f"Total number: {len(items)}\n\n")
-
-    # users = [item.user.login for item in items]
-    # user_counts = {user: users.count(user) for user in set(users)}
-    # f.write("Number of items per user:\n\n")
-    # for user, count in user_counts.items():
-    #     f.write(f"- {user}: {count}\n")
+    logger.info("Generated markdown for issues and pull requests successfully.")
+    return markdown_content
 
 
-def save_to_markdown(logger, today, issues, prs, graph_filename):
-    logger.info("Saving issues and pull requests to markdown...")
-    with open(f"{today}/{today}.md", "w") as f:
-        # generate_graphs_and_statistics(logger, today, issues, prs)
-
-        # f.write("\n\n## Graphs\n\n")
-        # f.write(f"![Issues and PRs by User](./{graph_filename})\n\n")
-
-        for state in ["Open", "Closed"]:
-            write_to_file(
-                f,
-                [i for i in issues if i.state.lower() == state.lower()],
-                state,
-                "Issues",
-            )
-            write_to_file(
-                f,
-                [p for p in prs if p.state.lower() == state.lower()],
-                state,
-                "Pull Requests",
-            )
-
-    logger.info("Saved issues and pull requests to markdown successfully.")
+def write_to_file(items, state, item_type):
+    content = f"\n\n# {state} {item_type}\n\n"
+    for item in items:
+        content += f"- [{item.title}]({item.html_url})\n"
+    return content
